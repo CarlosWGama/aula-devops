7886d9542c2b3e929e5f868ff7d1fa791877c812
CarlosWGama <carloswgama@gmail.com>
2024-01-10 16:32:51 -0300

    Testes

--- src/tests/login.spec.js
+++ src/tests/login.spec.js
@@ -1,1 +1,31 @@
-// https://blog.logrocket.com/testing-website-selenium-docker/
+const { Builder, By } = require('selenium-webdriver');
+
+describe('login', () => {
+  
+    test('logado com sucesso', async () => {
+        const driver = await new Builder().forBrowser('MicrosoftEdge').build();
+        await driver.get(__dirname+'/../../index.html');
+        //Login
+        await driver.findElement(By.id('email')).sendKeys('teste@teste.com');
+        await driver.findElement(By.id('password')).sendKeys('123456');
+        await driver.findElement(By.xpath('/html/body/form/p[4]/input')).click();
+        const url = await driver.getCurrentUrl();
+        const endpoint = url.split('/').pop();
+        expect(endpoint).toEqual('welcome.html')
+        await driver.quit();
+    })
+
+    test('falha ao logar', async () => {
+        const driver = await new Builder().forBrowser('MicrosoftEdge').build();
+        await driver.get(__dirname+'/../../index.html');
+        //Login
+        await driver.findElement(By.id('email')).sendKeys('errado@teste.com');
+        await driver.findElement(By.id('password')).sendKeys('123456');
+        await driver.findElement(By.xpath('/html/body/form/p[4]/input')).click();
+    
+        const error = await driver.findElement(By.id('error'));
+        expect(error).toBeDefined()
+    
+        await driver.quit();
+    })
+})
